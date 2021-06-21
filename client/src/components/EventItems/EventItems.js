8c47ba54e468d44cee253fd2f098b0de5923528f
Andrew Persaud <andreew.j.persaud@gmail.com>
2021-06-20 23:08:37 -0400

    Restyled profile screen, event list and items, added some filtering logic to event list

--- client/src/components/EventItems/EventItems.js
+++ client/src/components/EventItems/EventItems.js
@@ -1,25 +1,28 @@
 import { Link } from 'react-router-dom';
+import date from 'date-and-time';
 import './EventItems.scss';
 
 function EventItems(props) {
-    const { display_id, end_time, image, name, start_time } = props.eventData;
-    const date = (new Date(start_time)).toDateString();
-    const start = (new Date(start_time)).toLocaleTimeString();
-    const end = (new Date(end_time)).toLocaleTimeString();
+    const { display_id, end_time, image, name, start_time, description } = props.eventData;
+    const eventDate = date.format((new Date(start_time)), 'ddd, MMM DD YYYY')
+    const startTime = date.format((new Date(start_time)), 'hh:mm A');
+    const endTime = date.format((new Date(end_time)), 'hh:mm A');
+    
+
     return (
-        <li className="event">
-            <p className="event__number">{props.number}.</p>
-            <h3 className="event__name">{name}</h3>
-            <h4 className="event__date">{date}</h4>
-            <p className="event__start">{start}</p>
-            <p className="event__end">{end}</p>
-            <Link to={`/edit/${display_id}`} className="event__edit">
-                <div className="event__edit-box">
-                    <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px" className="event__edit-icon">
-                        <path d="M14.076 5.924L3 17 3 21 7 21 18.076 9.924z"/>
-                        <path d="M17.086 2.379H20.5V8.036H17.086z" transform="rotate(-45.001 18.793 5.207)"/>
-                    </svg>
-                </div>
+        <li>
+            <Link to={`/edit/${display_id}`} className="event">
+            <img src={image === "" ? "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg" : image} className="event__image" />
+            <h3 className="event__name">Event: {name}</h3>
+            <h4 className="event__date">Date: {eventDate}</h4>
+            <p className="event__time">Time: {startTime} - {endTime}</p>
+            <p className="event__description">{description}</p> 
+            <div className="event__edit-box">
+                <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px" className="event__edit-icon">
+                    <path d="M14.076 5.924L3 17 3 21 7 21 18.076 9.924z"/>
+                    <path d="M17.086 2.379H20.5V8.036H17.086z" transform="rotate(-45.001 18.793 5.207)"/>
+                </svg>
+            </div>
             </Link>
         </li>
     )
